e03d60e030bdae87ab5be16bf5ab03febc1eb20e
Nazar <nazaranddobriy@gmail.com>
2024-01-28 16:28:21 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', ws => {
+  console.log('A new client connected!');
+  ws.send('Welcome new client!');
+  ws.on('message', message => {
+    console.log('received: %s', message);
+  });
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
