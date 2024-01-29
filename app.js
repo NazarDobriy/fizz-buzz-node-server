bfcda310855ec0d15e7d0763a45f20a4042b734f
Nazar <nazaranddobriy@gmail.com>
2024-01-29 21:07:23 +0200

    fix: change place

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const http = require('https');
+const https = require('https');
 const path = require('path');
 const WebSocket = require('ws');
 
-const server = http.createServer({
+const server = https.createServer({
     key: fs.readFileSync(path.resolve('cert/key.pem')),
     cert: fs.readFileSync(path.resolve('cert/cert.pem'))
 }, app);
 
 const wss = new WebSocket.Server({ server });
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
 
 wss.on('connection', ws => {
   console.log('A new client connected!');
@@ -18,11 +23,4 @@ wss.on('connection', ws => {
   ws.on('message', message => {
     console.log('received: %s', message);
   });
-});
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
