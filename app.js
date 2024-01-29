1293f2be04cd79a26a5f08bbb0e48486846909a5
Nazar <nazaranddobriy@gmail.com>
2024-01-29 12:56:27 +0200

    fix: test

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const https = require('https');
 const path = require('path');
 const WebSocket = require('ws');
 
-const server = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app);
+// const server = https.createServer({
+//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+// }, app);
 
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
 
-wss.on('connection', ws => {
-  console.log('A new client connected!');
-  ws.send('Welcome new client!');
-  ws.on('message', message => {
-    console.log('received: %s', message);
-  });
-});
+// wss.on('connection', ws => {
+//   console.log('A new client connected!');
+//   ws.send('Welcome new client!');
+//   ws.on('message', message => {
+//     console.log('received: %s', message);
+//   });
+// });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
