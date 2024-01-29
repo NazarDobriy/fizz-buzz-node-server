e2f18a9893e12832c699e163f78592202b9e3a3a
Nazar <nazaranddobriy@gmail.com>
2024-01-29 13:18:32 +0200

    fix: change port

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
