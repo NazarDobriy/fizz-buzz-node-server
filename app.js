fc93aeb933af530727e45d1c3b2c5a31dd7cdc84
Nazar <nazaranddobriy@gmail.com>
2024-01-29 13:21:09 +0200

    fix: return changes

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
-// const fs = require('fs');
-// const https = require('https');
-// const path = require('path');
-// const WebSocket = require('ws');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const WebSocket = require('ws');
 
-// const server = https.createServer({
-//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-// }, app);
+const server = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app);
 
-// const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server });
 
-// wss.on('connection', ws => {
-//   console.log('A new client connected!');
-//   ws.send('Welcome new client!');
-//   ws.on('message', message => {
-//     console.log('received: %s', message);
-//   });
-// });
+wss.on('connection', ws => {
+  console.log('A new client connected!');
+  ws.send('Welcome new client!');
+  ws.on('message', message => {
+    console.log('received: %s', message);
+  });
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
