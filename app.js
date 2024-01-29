1c97fa5021fe8830b91eb3792690bf55b03d3836
Nazar <nazaranddobriy@gmail.com>
2024-01-29 13:40:01 +0200

    fix: change to server run

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ wss.on('connection', ws => {
 app.get('/', (req, res) => res.send('Hello World!'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
